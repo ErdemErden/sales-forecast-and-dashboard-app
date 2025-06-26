28b5ca101bc159f4e9c2edd11f5244ba3cf6beb4
ErdemErden <erdemn33@gmail.com>
2025-06-26 12:32:39 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Sales Forecast Tool", layout="wide")
 
-st.markdown("<h1 style='text-align: center;'>📊 Sales Forecast Tool</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>📊 Sales Forecasting Tool</h1>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center;'>Please choose one of the forecast types below:</h4>", unsafe_allow_html=True)
 
 col1, col2 = st.columns(2)
